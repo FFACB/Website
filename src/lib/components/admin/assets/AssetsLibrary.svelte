7326565c71d75d8ad5bd24d11274b009060d2c44
GuillianV <guillianvibert90@gmail.com>
2025-04-24 14:35:03 +0200

    feat: dep

--- src/lib/components/admin/assets/AssetsLibrary.svelte
+++ src/lib/components/admin/assets/AssetsLibrary.svelte
@@ -187,7 +187,7 @@
 		enctype="multipart/form-data"
 		use:enhance={submitFindall}
 	>
-		<button bind:this={refreshButton} class="hidden" type="submit" form="findall" value="Refresh"
+		<button bind:this={refreshButton} aria-label="Rafraichir" class="hidden" type="submit" form="findall" value="Refresh"
 		></button>
 	</form>
 
