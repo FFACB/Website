313ce408e9dc158451b2d119e5cc4481261bb2b6
GuillianV <guillianvibert90@gmail.com>
2025-04-24 16:34:41 +0200

    fix: auth

--- conf/auth/auth.js
+++ conf/auth/auth.js
@@ -1,27 +1,40 @@
 import { Lucia } from 'lucia';
-
 import { PrismaAdapter } from '@lucia-auth/adapter-prisma';
 import { logger } from '../../modules/server/logs';
 import { prisma } from '../../modules/server/database';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { SECRET_ADMIN_EMAIL, SECRET_ADMIN_PASSWORD, NODE_ENV } = process.env;
 
 /**
  * @description Initialisation de l'authentification administrateur
- * @param {string} environement
- * @param {string} adminEmail
- * @param {string} adminPassword
- *
+
  * @example initialize('DEV','admin','admin123')
  * @returns {Promise<void>}
  */
 
-export async function initialize(environement, adminEmail, adminPassword) {
+export async function initialize() {
 	try {
+		console.log('Initialisation authentification');
+		logger.debug('Initialisation authentification');
+
+		if (
+			typeof SECRET_ADMIN_EMAIL !== 'string' ||
+			typeof SECRET_ADMIN_PASSWORD !== 'string' ||
+			typeof NODE_ENV !== 'string'
+		) {
+			logger.fatal("Variables d'environement manquantes");
+			process.exit(1);
+		}
+
 		const adapter = new PrismaAdapter(prisma.session, prisma.user);
 		new Lucia(adapter, {
 			sessionCookie: {
 				attributes: {
-					secure: environement == 'PROD'
+					secure: NODE_ENV == 'PROD'
 				}
 			},
 
@@ -35,8 +48,8 @@ export async function initialize(environement, adminEmail, adminPassword) {
 
 		await prisma.user.create({
 			data: {
-				username: adminEmail,
-				password: adminPassword
+				username: SECRET_ADMIN_EMAIL,
+				password: SECRET_ADMIN_PASSWORD
 			}
 		});
 	} catch (err) {
