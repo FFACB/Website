e21832b2b5a7388abcfe8b297b1f43e13061bc5b
GuillianV <guillianvibert90@gmail.com>
2025-07-27 15:38:10 +0200

    fix prerender

--- src/routes/(front)/+page.server.ts
+++ src/routes/(front)/+page.server.ts
@@ -1,6 +1,8 @@
 import type { PageServerLoad } from './$types';
 import { getParametre } from '$lib/server/parametres/parametres';
 
+export const prerender = 'auto';
+
 export const load: PageServerLoad = async () => {
 	return {
 		parametres: {
