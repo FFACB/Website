a8a022e266f52c67b335dee9522a8ebc88ef6889
GuillianV <guillianvibert90@gmail.com>
2025-09-03 13:43:01 +0200

    feat: professionnel

---
+++ src/routes/(front)/professionnels/+page.svelte
@@ -0,0 +1,479 @@
+<div
+	class="w-full h-[calc(100vh-112px)] flex flex-col justify-end items-start relative shadow-inner"
+>
+	<img
+		class="w-full h-full object-cover bg-cover absolute"
+		src="/images/placeholder.webp"
+		alt="federation"
+	/>
+	<div class="relative ml-16 flex h-[90%] w-2/5 flex-col justify-end items-center">
+		<img
+			class="absolute w-full bottom-0 h-full"
+			alt="background"
+			src="/images/FFACB_forme_entete_page_blue.png"
+		/>
+		<div class="z-10 p-16 flex flex-col justify-center">
+			<h1 class="w-full h1-white font-extrabold mb-8">
+				Comment fonctionne une coopérative d'artisans ?
+			</h1>
+		</div>
+	</div>
+</div>
+
+<div class="font-Funnel Sans w-full flex flex-row">
+	<div class="bg-white w-1/2 p-16 pl-32 pr-32">
+		L'article 1 er de la loi n° 83-657 du 20 juillet 1983 stipule que « les Sociétés Coopératives
+		Artisanales ont pour objet la réalisation de toutes opérations et la prestation de tous services
+		susceptibles de contribuer, directement ou indirectement, au développement des activités
+		artisanales de leurs associés ainsi que l'exercice en commun de ces activités. »<br /><br />
+		Les coopératives du bâtiment sont constituées sous forme de
+		<strong>Société à Responsabilité Limitée</strong> (SARL) ou de <strong>Société Anonyme</strong>
+		(SA). La coopérative doit être fondée sur un principe d'égalité des droits qui s'exprime sous la
+		formule <strong class="inline-block">« un associé = une voix »</strong>.
+	</div>
+	<div class="bg-white w-1/2 p-16">
+		Le plus souvent les coopératives et groupements d'artisans sont composés d'une équipe
+		opérationnelle réunissant les différents corps d'état, en moyenne une dizaine d'entreprises, qui
+		traitent dix à trente chantiers par an, voire plus selon le volume d'activité. Par ailleurs, ces
+		entreprises exercent une activité en propre, suivant leur métier, en dehors de la coopérative.<br
+		/><br />
+		Les chantiers des coopératives sont soumis en permanence aux autocontrôles et présentent le plus
+		faible taux de sinistralité dans le secteur de la maison individuelle, ce qui représente un véritable
+		argument pour sécuriser le client dans son projet.
+	</div>
+</div>
+
+<div class="flex bg-white">
+	<div class="flex flex-col w-1/2 pr-8 p-16">
+		<h2 class="h2-blue font-bold">Pourquoi se constituer en coopérative avec la FFACB ?</h2>
+		<br />
+		<p class="">
+			Avant tout, parce <strong> l'union fait la force</strong>. Les artisans regroupés sous forme
+			de groupements ou de coopératives l'ont compris. Ils unissent ainsi leurs forces et leurs
+			compétences pour remporter de nouveaux marchés, construire des maisons individuelles ou
+			rénover l'habitat existant.
+		</p>
+		<br />
+		<h3 class="uppercase font-bold text-xl">
+			Être membre de la FFACB permet aux coopératives adhérentes de :
+		</h3>
+		<ul class="font-semibold text-lg text-blue mt-4">
+			<li class="mt-2">
+				<span class="w-2 h-2 inline-block bg-blue rounded-full mr-2"></span>Travailler en commun
+				pour gagner des marchés.
+			</li>
+			<li class="mt-2">
+				<span class="w-2 h-2 inline-block bg-blue rounded-full mr-2"></span>Gagner du temps et de la
+				productivité.
+			</li>
+			<li class="mt-2">
+				<span class="w-2 h-2 inline-block bg-blue rounded-full mr-2"></span>Mutualiser et offrir des
+				services performants.
+			</li>
+			<li class="mt-2">
+				<span class="w-2 h-2 inline-block bg-blue rounded-full mr-2"></span>Rompre l'isolement.
+			</li>
+			<li class="mt-2">
+				<span class="w-2 h-2 inline-block bg-blue rounded-full mr-2"></span>Faire fonctionner la
+				solidarité.
+			</li>
+		</ul>
+		<br />
+		<p class="">
+			La coopérative artisanale est un outil de développement qui permet aux entreprises et artisans
+			de multiplier leurs opportunités de marchés tout en conservant leur autonomie. Il suffit
+			d'accepter un pouvoir central de coordination, dont les artisans membres de la coopérative
+			restent les principaux acteurs.
+		</p>
+		<br />
+		<p>
+			Si la force des petites et moyennes entreprises repose sur leur savoir-faire, leur compétences
+			spécifiques et leur adaptabilité, vous devez en tant qu'artisan, maîtriser des domaines aussi
+			larges que le commercial, les ressources humaines ou encore la gestion administrative et
+			l'informatique. Des responsabilités et une charge de travail pesantes au quotidien.
+		</p>
+		<br />
+		<p>
+			En choisissant d'exercer une partie de votre activité au sein d'une coopérative artisanale,
+			vous profitez de la force de frappe et de l'appui de cette structure organisationnelle. Les
+			entreprises coopératives d'une même famille adhèrent à une fédération qui les représente et
+			leur apporte un ensemble de conseils et d'assistance, en vue d'assurer leur bon
+			fonctionnement. C'est le rôle de la FFACB.
+		</p>
+	</div>
+	<div class="flex flex-col w-1/2 mt-64">
+		<div class="bg-dark h-full relative">
+			<img
+				src="/images/profesionnel-droit.png"
+				alt="professionnel"
+				class="w-auto h-full object-contain bg-contain absolute -translate-y-1/3 right-0 top-0"
+			/>
+
+			<div class="flex flex-col w-full h-1/2 relative p-16">
+				<div class="relative h-full">
+					<img
+						src="/images/personnage_temoignage_attentephoto.png"
+						alt="FFACB_forme_entete_page_blue.png"
+						class="w-1/4 h-1-4 object-cover bg-cover absolute bottom-0 left-0"
+					/>
+				</div>
+			</div>
+			<div class="flex flex-col w-full h-1/2 relative pl-16 pr-16">
+				<p class="text-white text-lg font-Funnel-Sans-Italic">
+					« En travaillant en coopérative, nous avons l'immense avantage de connaitre parfaitement
+					notre réseau d'artisans et de maîtriser notre sujet de A à Z. Nous ne sommes pas juste des
+					assembleurs de compétences, nous mettons en œuvre nos connaissances et notre expertise de
+					la construction. En étant des artisans constructeurs et pas des constructeurs faisant
+					appel à des artisans, nous avons une logique inverse. Après le soutien de la FFACB pour
+					nous structurer est essentiel. Passer en CCMI est une démarche chronophage et coûteuse.
+					C'est un enrichissement de pouvoir compter sur l'expérience et le savoir des
+					professionnels de la FFACB qui sont extrêmement pointus sur le sujet »,
+				</p>
+				<br />
+				<p class="text-blue text-sm font-semibold">
+					Anne Lang, gérante d'Avenir et Bois, constructeur de maisons passives en bois en Alsace,
+					membre de la coopérative ARTCOPA.
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="bg-[#DCEFFC] text-center p-16 flex flex-col justify-center items-center">
+	<h2 class="h2-blue font-bold">
+		Quels sont les avantages à se constituer en coopérative et à adhérer à la fédération ?
+	</h2>
+	<br /><br />
+	<div class="flex flex-row">
+		<div class="flex flex-col m-2 p-8 text-white bg-dark rounded-3xl basis-1/3">
+			<h3 class="uppercase font-bold text-xl">Des principes fondateurs</h3>
+			<br />
+			<ul class="font-light">
+				<li>
+					<span class="w-2 h-2 inline-block bg-white rounded-full mr-2"></span>Exonération de
+					l'impôt sur les sociétés (IS) *
+				</li>
+				<li>
+					<span class="w-2 h-2 inline-block bg-white rounded-full mr-2"></span>Exonération de
+					Contribution Économique Territoriale (CFE et CVAE), ancienne Taxe Professionnelle *
+				</li>
+				<li>
+					<span class="w-2 h-2 inline-block bg-white rounded-full mr-2"></span>Exonération de la
+					Taxe d'Apprentissage *
+				</li>
+				<li>
+					<span class="w-2 h-2 inline-block bg-white rounded-full mr-2"></span>Exonération de la
+					Contribution Sociale de Solidarité des Sociétés (C3S) * = sauf pour la quote-part de
+					résultat issue de l'activité réalisée avec les sous-traitants.
+				</li>
+			</ul>
+		</div>
+		<div class="flex flex-col m-2 p-8 text-white bg-red rounded-3xl basis-1/3">
+			<h3 class="uppercase font-bold text-xl">Les avantages juridiques</h3>
+			<br />
+			<ul class="font-light">
+				<li>
+					<span class="w-2 h-2 inline-block bg-white rounded-full mr-2"></span>Aide à la rédaction
+					et contrôle du Contrat de Construction de Maison Individuelle (CCMI).
+				</li>
+				<li>
+					<span class="w-2 h-2 inline-block bg-white rounded-full mr-2"></span>Accompagnement
+					juridique, fiscal et comptable.
+				</li>
+				<li>
+					<span class="w-2 h-2 inline-block bg-white rounded-full mr-2"></span>Défense des intérêts
+					des artisans coopérateurs.
+				</li>
+			</ul>
+		</div>
+		<div class="flex flex-col m-2 p-8 text-white bg-blue rounded-3xl basis-1/3">
+			<h3 class="uppercase font-bold text-xl">Des atouts majeurs</h3>
+			<br />
+			<ul class="font-light">
+				<li>
+					<span class="w-2 h-2 inline-block bg-white rounded-full mr-2"></span>Établissement des
+					statuts et du règlement intérieur.
+				</li>
+				<li>
+					<span class="w-2 h-2 inline-block bg-white rounded-full mr-2"></span>Étude du budget
+					prévisionnel.
+				</li>
+				<li>
+					<span class="w-2 h-2 inline-block bg-white rounded-full mr-2"></span>Tenue des assemblées
+					générales.
+				</li>
+
+				<li>
+					<span class="w-2 h-2 inline-block bg-white rounded-full mr-2"></span>Indépendance
+					vis-à-vis de la sous-traitance.
+				</li>
+
+				<li>
+					<span class="w-2 h-2 inline-block bg-white rounded-full mr-2"></span>Mise en place de
+					nombreux outils de travail efficaces.
+				</li>
+
+				<li>
+					<span class="w-2 h-2 inline-block bg-white rounded-full mr-2"></span>Maintien et contrôle
+					des marges.
+				</li>
+
+				<li>
+					<span class="w-2 h-2 inline-block bg-white rounded-full mr-2"></span>Formations adaptées.
+				</li>
+
+				<li>
+					<span class="w-2 h-2 inline-block bg-white rounded-full mr-2"></span>Rencontres et
+					partages d'expérience.
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<div class="bg-dark flex justify-center items-center pt-9 pb-9">
+	<div class="h-auto">
+		<img class="h-36" src="/images/personnage_temoignage_attentephoto.svg" alt="persona" />
+	</div>
+	<div class="w-3/5 ml-7">
+		<div class="font-Funnel-Sans-Italic font-medium text-lg text-white">
+			« Parmi les principaux avantages de l'affiliation à la FFACB il y a évidemment le fait de
+			profiter du contrat de construction élaboré dans les règles de l'art. Mais au-delà de ça, les
+			services de la FFACB nous permettent de profiter de conseils juridiques très pointus, d'être
+			au courant des dernières réglementations et d'avoir toujours un interlocuteur pour répondre
+			rapidement aux problématiques du quotidien »,
+		</div>
+		<br>
+		<div class="text-blue text-sm">
+			Benjamin Houseaux, Vision Bois, constructeur de maisons en ossature bois basé à Chambéry (73)
+			et membre de la coopérative Tout Habitat.
+		</div>
+	</div>
+</div>
+
+<div class="bg-white text-center p-16 flex flex-col justify-center items-center">
+	<h2 class="h2-blue font-bold">Quels sont les services proposés par la FFACB ?</h2>
+	<br /><br />
+	<div class="flex flex-row flex-wrap justify-center items-center">
+		<div class="p-2 basis-1/3">
+			<div
+				class="flex flex-col justify-center items-center m-2 p-8 bg-[#DCEFFC] rounded-3xl basis-1/3"
+			>
+				<h3 class="uppercase font-bold text-dark text-xl">
+					Un accompagnement juridique, fiscal et comptable
+				</h3>
+				<br />
+				<div class="w-max m-2 p-4 pt-2 pb-2 font-bold text-white bg-red rounded-full">
+					<span class="uppercase cursor-pointer">En savoir plus</span>
+				</div>
+			</div>
+		</div>
+
+		<div class="p-2 basis-1/3">
+			<div
+				class="flex flex-col justify-center items-center m-2 p-8 bg-[#DCEFFC] rounded-3xl basis-1/3"
+			>
+				<h3 class="uppercase font-bold text-dark text-xl">
+					Un service de gestion des produits d'assurances
+				</h3>
+				<br />
+				<div class="w-max m-2 p-4 pt-2 pb-2 font-bold text-white bg-red rounded-full">
+					<span class="uppercase cursor-pointer">En savoir plus</span>
+				</div>
+			</div>
+		</div>
+
+		<div class="p-2 basis-1/3">
+			<div
+				class="flex flex-col justify-center items-center m-2 p-8 bg-[#DCEFFC] rounded-3xl basis-1/3"
+			>
+				<h3 class="uppercase font-bold text-dark text-xl">
+					Un service réviseurs en lien avec l'ARCAB
+				</h3>
+				<br />
+				<div class="w-max m-2 p-4 pt-2 pb-2 font-bold text-white bg-red rounded-full">
+					<span class="uppercase cursor-pointer">En savoir plus</span>
+				</div>
+			</div>
+		</div>
+
+		<div class="p-2 basis-1/3">
+			<div
+				class="flex flex-col justify-center items-center m-2 p-8 bg-[#DCEFFC] rounded-3xl basis-1/3"
+			>
+				<h3 class="uppercase font-bold text-dark text-xl">
+					Un service de formations en partenariat avec l'AFCAB
+				</h3>
+				<br />
+				<div class="w-max m-2 p-4 pt-2 pb-2 font-bold text-white bg-red rounded-full">
+					<span class="uppercase cursor-pointer">En savoir plus</span>
+				</div>
+			</div>
+		</div>
+
+		<div class="p-2 basis-1/3">
+			<div
+				class="flex flex-col justify-center items-center m-2 p-8 bg-[#DCEFFC] rounded-3xl basis-1/3"
+			>
+				<h3 class="uppercase font-bold text-dark text-xl">Un partage d'expériences</h3>
+				<br />
+				<div class="w-max m-2 p-4 pt-2 pb-2 font-bold text-white bg-red rounded-full">
+					<span class="uppercase cursor-pointer">En savoir plus</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- TODO -->
+
+<div class="bg-red text-center p-16 flex flex-col justify-center items-center">
+	<h3 class="h2-blue text-white font-bold">Comment créer une coopérative avec la FFACB ?</h3>
+
+	<p class="font-semibold text-lg text-white mt-4 mb-6">
+		Si vous envisagez de créer une coopérative artisanale du bâtiment, la première chose à faire est
+		de contacter la FFACB, par mail de préférence à : <span class="text-dark underline"
+			><a href="mailto:infos@ffacb.com">infos@ffacb.com</a></span
+		>
+	</p>
+	<p class="font-light text-lg text-white mt-4 mb-6">
+		Après transmission des éléments clés pour la constitution de la coopérative permettant aux
+		candidats de prendre du recul sur leur démarche, la FFACB propose une formation axée sur le
+		statut coopératif afin de bien maîtriser l'angle et la composition de la structure à venir.
+	</p>
+	<p class="font-light text-lg text-white mt-4 mb-6">
+		Une fois le projet enclenché, la FFACB prépare et établit les statuts à déposer puis transmet
+		les dossiers aux partenaires.
+	</p>
+
+	<div class="w-max m-2 p-4 pt-2 pb-2 font-bold text-white bg-dark rounded-full">
+		<a class="uppercase" href="/contact">Nous contacter</a>
+	</div>
+</div>
+
+<div class="bg-white text-center p-16 flex flex-col justify-center items-center">
+	<h2 class="h2-blue font-bold">Principes fondateurs de la FFACB</h2>
+	<br /><br />
+	<div class="flex flex-row flex-wrap justify-center items-center w-full">
+		<div class="p-4 basis-1/2">
+			<div class="flex flex-col justify-center items-start m-2 p-8 bg-[#DCEFFC] rounded-3xl w-full">
+				<h3 class="uppercase font-bold text-blue text-xl">Liberté</h3>
+
+				<ul class="  text-dark mt-4 text-left">
+					<li class="mt-2">
+						<span class="w-2 h-2 inline-block bg-blue rounded-full mr-2"></span>Liberté d'adhésion
+						et de retrait
+					</li>
+					<li class="mt-2">
+						<span class="w-2 h-2 inline-block bg-blue rounded-full mr-2"></span>Les entreprises se
+						choisissent librement
+					</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="p-4 basis-1/2">
+			<div class="flex flex-col justify-center items-start m-2 p-8 bg-[#DCEFFC] rounded-3xl w-full">
+				<h3 class="uppercase font-bold text-blue text-xl">Droits</h3>
+
+				<ul class="  text-dark mt-4 text-left">
+					<li class="mt-2">
+						<span class="w-2 h-2 inline-block bg-blue rounded-full mr-2"></span>Les entreprises
+						disposent de droits égaux : pour l'accès aux services* et les prises de décision (1
+						entreprise = 1 voix)
+					</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="p-4 basis-1/2">
+			<div class="flex flex-col justify-center items-start m-2 p-8 bg-[#DCEFFC] rounded-3xl w-full">
+				<h3 class="uppercase font-bold text-blue text-xl">Services</h3>
+
+				<ul class="  text-dark mt-4 text-left">
+					<li class="mt-2">
+						<span class="w-2 h-2 inline-block bg-blue rounded-full mr-2"></span>Seules les
+						entreprises adhérentes ont accès aux services de la coopérative**
+					</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="p-4 basis-1/2">
+			<div class="flex flex-col justify-center items-start m-2 p-8 bg-[#DCEFFC] rounded-3xl w-full">
+				<h3 class="uppercase font-bold text-blue text-xl">Obligations</h3>
+
+				<ul class="  text-dark mt-4 text-left">
+					<li class="mt-2">
+						<span class="w-2 h-2 inline-block bg-blue rounded-full mr-2"></span>Utiliser les
+						services de la coopérative
+					</li>
+					<li class="mt-2">
+						<span class="w-2 h-2 inline-block bg-blue rounded-full mr-2"></span>Participer à la
+						gestion de celle-ci
+					</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="p-4 basis-1/2">
+			<div class="flex flex-col justify-center items-start m-2 p-8 bg-[#DCEFFC] rounded-3xl w-full">
+				<h3 class="uppercase font-bold text-blue text-xl">Equité</h3>
+
+				<ul class="  text-dark mt-4 text-left">
+					<li class="mt-2">
+						<span class="w-2 h-2 inline-block bg-blue rounded-full mr-2"></span>Les entreprises
+						participent de façon équitable aux résultats, au prorata de leur activité avec la
+						coopérative***
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div>
+		<ul class="font-light text-xs mt-4 mb-6">
+			<li>* sauf pour la 3 ème catégorie.</li>
+			<li>** exclusivisme sauf dérogation pour les Tiers Non Associés.</li>
+			<li>*** (sauf pour la 3 ème catégorie et les Tiers Non Associés).</li>
+		</ul>
+	</div>
+	<div>
+		À travers ses principes fondateurs, la FFACB assure une meilleure défense des intérêts des
+		coopératives artisanales, notamment vis-à-vis des pouvoirs publics et au regard des projets de
+		lois qui peuvent impacter la vie des artisans. En assurant un accompagnement régulier et une
+		veille permanente sur les évolutions législatives, se regrouper en fédération permet aux
+		coopératives artisanales de se tenir à jour et de se développer dans un cadre juridique fiable.
+	</div>
+	<br />
+
+	<div>
+		Les valeurs de la coopérative artisanale, fondées sur des principes d'économie sociale et
+		solidaire, permettent aux artisans d'unir leurs forces et de continuer à exister dans un marché
+		très concurrentiel.
+	</div>
+</div>
+
+<div class="bg-dark flex justify-center items-center pt-9 pb-9">
+	<div class="h-auto">
+		<img class="h-36" src="/images/personnage_temoignage_attentephoto.svg" alt="persona" />
+	</div>
+	<div class="w-3/5 ml-7">
+		<div class="text-white">
+			La solidarité et la mutualisation des moyens sont des atouts non négligeables pour faire face
+			à la concurrence :
+		</div>
+		<br />
+		<div class="font-Funnel-Sans-Italic font-medium text-lg text-white">
+			« Parmi les principaux avantages de l'affiliation à la FFACB il y a évidemment le fait de
+			profiter du contrat de construction élaboré dans les règles de l'art. Mais au-delà de ça, les
+			services de la FFACB nous permettent de profiter de conseils juridiques très pointus, d'être
+			au courant des dernières réglementations et d'avoir toujours un interlocuteur pour répondre
+			rapidement aux problématiques du quotidien »
+		</div>
+		<br />
+		<div class="text-blue text-sm">
+			Benjamin Houseaux, Vision Bois, constructeur de maisons en ossature bois basé à Chambéry (73)
+			et membre de la coopérative Tout Habitat.
+		</div>
+	</div>
+</div>
