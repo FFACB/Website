a910e7f128bafd251f5285b4b38068ae28773fe0
GuillianV <guillianvibert90@gmail.com>
2025-11-04 14:59:25 +0100

    fix prerender

--- src/routes/(front)/contact/+page.server.ts
+++ src/routes/(front)/contact/+page.server.ts
@@ -1,7 +1,7 @@
 import type { PageServerLoad } from './$types';
 import { getParametre } from '$lib/server/parametres/parametres';
 
-export const prerender = 'auto';
+export const prerender = false;
 
 export const load: PageServerLoad = async () => {
 	const PUBLIC_RECAPCHA_SITEKEY = await getParametre('PUBLIC_RECAPCHA_SITEKEY');
